ae275321dee0961697d775a251dbe8c30b51bc9c
Davide Silva <davidesilva@hey.com>
2021-10-23 19:17:21 +0100

    initial smart contract implementation

--- blockchain/contracts/MetaverseTrain.sol
+++ blockchain/contracts/MetaverseTrain.sol
@@ -2,49 +2,134 @@
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MetaverseTrain {
-  uint256 private _golden_tickets;
+contract MetaverseTrain is ERC1155, Ownable {
+  uint256 public goldenTicketsSupply;
+  uint256 public ticketPrice;
+  bool public ticketSale;
+  uint256 public tokenId;
+  uint256 public voteId;
 
-  constructor() {
-    _golden_tickets = 500;
-    mint_golden_ticket()
+  struct VoteCounter {
+    uint8 left;
+    uint8 right;
+    string leftDestination;
+    string rightDestination;
   }
 
-  function mint_golden_ticket() {}
+  mapping(uint256 => VoteCounter) public choices;
+  mapping(uint256 => string) public destinations;
 
-  function claim_golden_ticket() payable {}
+  constructor(
+    uint256 _goldenTicketsSupply,
+    uint256 _ticketPrice,
+    string memory uri
+  ) ERC1155(uri) {
+    goldenTicketsSupply = _goldenTicketsSupply;
+    ticketPrice = _ticketPrice;
+    ticketSale = true;
+    tokenId = 1;
+    voteId = 1;
 
-  function vote_start(options) {}
-
-  function vote(pick) {
-    put_golden_ticket_in_escrow()
-    set_pick(pick)
+    _mintGoldenTicket();
   }
 
-  function settlement() {
-    list_contract_golden_tickets()
-    vote_start(new_options)
+  event GoldenTicketAssigned(address buyer);
+  event Voted(uint256 voteId, uint8 pick);
+  event VoteStart(uint256 voteId, string firstChoice, string rightChoice);
+  event ChooChoo();
+
+  function _mintGoldenTicket() internal {
+    _mint(address(this), tokenId, goldenTicketsSupply, "");
   }
 
-  function claim() {
-    return_ticket()
-    redeem_nft()
+  function buyGoldenTicket() public payable {
+    uint256 ticketsAvailable = balanceOf(address(this), 1);
+
+    bool oldTicketSale = ticketSale;
+
+    if (ticketsAvailable == 1) {
+      ticketSale = false;
+    }
+
+    require(oldTicketSale, "Sale is not available");
+
+    require(msg.value >= ticketPrice, "Amount sent is lower than ticket price");
+
+    _safeTransferFrom(address(this), msg.sender, 1, 1, "");
+    emit GoldenTicketAssigned(msg.sender);
   }
 
-  function return_ticket() {}
-
-  function redeem_nft() {}
-  function mint() {}
-
-  function transfer_nft() {}
-
-  function payout() {}
-
-
-  function the_most_important_function_of_all() {
-    choo_choo()
+  function balanceOf(address _address, uint256 id) public view override returns (uint256) {
+    return ERC1155.balanceOf(_address, id);
   }
+
+  function voteStart(
+    uint256 _voteId,
+    string memory firstChoice,
+    string memory secondChoice
+  ) public {
+    choices[_voteId] = VoteCounter(0, 0, firstChoice, secondChoice);
+
+    emit VoteStart(_voteId, firstChoice, secondChoice);
+  }
+
+  function vote(uint8 pick) public {
+    // putGoldenTicketInEscrow();
+
+    setPick(pick);
+    emit Voted(voteId, pick);
+  }
+
+  function setPick(uint8 pick) public {
+    // pick 0 or 1
+    require(pick <= 1, "Wrong pick");
+
+    if (pick == 0) {
+      choices[voteId].left = choices[voteId].left + 1;
+    } else {
+      choices[voteId].right = choices[voteId].right + 1;
+    }
+  }
+
+  //   function settlement() {
+  //     const currentVoteId = voteId;
+  //
+  //     // destinations[currentVoteId]
+  //
+  //     voteId++;
+  //
+  //     emit VoteEnd(currentVoteId, winner);
+  //
+  //
+  //
+  //     listContractGoldenTickets();
+  //
+  //     voteStart(voteId, new_first_choice, new_second_choice);
+  //   }
+  //
+  //   function listContractGoldenTickets() {
+  //
+  //   }
+  //
+  //   function claim() {
+  //     return_ticket()
+  //     redeem_nft()
+  //   }
+  //
+  //   function return_ticket() {}
+  //
+  //   function redeem_nft() {}
+  //
+  //   function mint() {}
+  //
+  //   function transfer_nft() {}
+  //
+  //   function payout() {}
+  //
+  //   function the_most_important_function_of_all() {
+  //     emit ChooChoo();
+  //   }
 }
-
-
